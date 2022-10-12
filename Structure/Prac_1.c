38fa4b88e0a5c6cc596c4b5a0f37654a091d8deb
eomseung-yeol <co6747@naver.com>
2022-10-12 22:14:14 +0900

    feat: Strcture prac2

--- Structure/Prac_1.c
+++ Structure/Prac_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+//구조체 선언.
 typedef struct {
     int id;
     int age;
@@ -7,14 +8,21 @@ typedef struct {
 
 int main() {
     Student test;
-    Student *ptr;
+    Student *ptr; 
 
     ptr = &test;
-    
+
+    // 구조체 변수를 포인터를 이용하여 값을 대입할 수 있다. 이떄 괄호를 사용하여 대입을 해야함. 
     (*ptr).id = 2016;
-    (*ptr).age = 25;
+    (*ptr).age = 25; 
 
     printf("id : %d, age : %d \n", test.id,test.age);
+
+    // 매번 괄호를 사용하면 귀찮기 때문에, -> 기호를 사용하여 포인터를 사용해 구조체 변수에 대입할 수 있음.
+    ptr->id = 2022;
+    ptr->age = 30;
+
+    printf("new id: %d, new age : %d\n", test.id,test.age);
 
     return 0;
     
